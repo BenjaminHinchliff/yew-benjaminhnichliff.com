9a809c784479a1f038b3de2a72e15d100a9c6819
Benjamin Hinchliff <benjamin.hinchliff@gmail.com>
2020-08-23 13:23:13 -0700

    add header code

--- app/src/lib.rs
+++ app/src/lib.rs
@@ -14,15 +14,12 @@ impl Component for Model {
     type Message = Msg;
     type Properties = ();
     fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
-        Self {
-            link,
-            value: 0,
-        }
+        Self { link, value: 0 }
     }
 
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
         match msg {
-            Msg::AddOne => self.value += 1
+            Msg::AddOne => self.value += 1,
         }
         true
     }
@@ -33,12 +30,31 @@ impl Component for Model {
 
     fn view(&self) -> Html {
         html! {
-            <div>
+            <div id="app">
+                <div id="intro">
+                    <div id="header">
+                        <h1>{ "Benjamin Hinchliff" }</h1>
+                        <p id="blerb">
+                            {normalize_whitespace("
+                            A skilled self-taught programmer with professional development
+                            experience; coding community leader, assisting at local elementary and
+                            middle school classes and camps and running self-founded high school
+                            coding club; long active with presiding and participating in many
+                            neighborhood activities through BSA Scouts.
+                            ")}
+                        </p>
+                    </div>
+                    <img id="portrait" src="./assets/self.jpeg" />
+                </div>
                 <button onclick=self.link.callback(|_| Msg::AddOne)>{ "+1" }</button>
                 <p>{ self.value }</p>
             </div>
         }
     }
+}
+
+fn normalize_whitespace(string: &str) -> String {
+    String::from(string).split_whitespace().collect::<Vec<&str>>().join(" ")
 }
 
 #[wasm_bindgen(start)]
