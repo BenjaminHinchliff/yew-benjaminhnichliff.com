684d4ac7ec78016e1c772eac43843d0e9d017359
Benjamin Hinchliff <benjamin.hinchliff@gmail.com>
2020-08-22 17:48:49 -0700

    configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
+const { PassThrough } = require('stream');
+
+
+const sourceDirectory = path.resolve(__dirname, 'public');
+
+module.exports = {
+    entry: path.join(sourceDirectory, 'index.js'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(sourceDirectory, 'index.ejs')
+        }),
+        new WasmPackPlugin({
+            crateDirectory: path.resolve(__dirname, 'app')
+        }),
+        new webpack.ProvidePlugin({
+            TextDecoder: ['text-encoding', 'TextDecoder'],
+            TextEncoder: ['text-encoding', 'TextEncoder']
+        })
+    ]
+}
