3d97889aeca8863fa7e2aa87da80f30cade6cd38
Benjamin Hinchliff <benjamin.hinchliff@gmail.com>
2020-08-24 15:39:18 -0700

    add time parsing to date

--- app/src/repos/repo.rs
+++ app/src/repos/repo.rs
@@ -1,4 +1,5 @@
 use yew::prelude::*;
+use chrono::prelude::*;
 use serde::{Serialize, Deserialize};
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -6,6 +7,7 @@ pub struct Repo {
     pub name: String,
     pub created_at: String,
     pub html_url: String,
+    pub description: Option<String>,
 }
 
 #[derive(Properties, Clone, PartialEq)]
@@ -13,6 +15,7 @@ pub struct Props {
     pub name: String,
     pub created_at: String,
     pub html_url: String,
+    pub description: Option<String>,
 }
 
 impl Component for Repo {
@@ -20,8 +23,8 @@ impl Component for Repo {
     type Properties = Props;
 
     fn create(props: Self::Properties, _link: ComponentLink<Self>) -> Self {
-        let Props { name, created_at, html_url } = props;
-        Repo { name, created_at, html_url }
+        let Props { name, created_at, html_url, description } = props;
+        Repo { name, created_at, html_url, description }
     }
 
     fn update(&mut self, _msg: Self::Message) -> ShouldRender {
@@ -36,8 +39,8 @@ impl Component for Repo {
         html! {
             <a class="repo" href=self.html_url.clone() rel="noopener noreferrer" target="_blank">
                 <h5 class="name">{ &self.name }</h5>
-                <h6 class="date">{ &self.created_at }</h6>
-                <p class="description">{ "(no description)" }</p>
+                <h6 class="date">{ self.created_at.parse::<DateTime<Utc>>().unwrap().format("%d/%m/%Y") }</h6>
+                <p class="description">{ self.description.as_ref().unwrap_or(&"(no description)".to_owned()) }</p>
             </a>
         }
     }
