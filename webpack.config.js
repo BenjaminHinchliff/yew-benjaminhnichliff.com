d9051c8fd7fb2b84253d0f84e4ecb72a58c7ff25
Benjamin Hinchliff <benjamin.hinchliff@gmail.com>
2020-08-23 21:44:56 -0700

    reformat webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,38 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const webpack = require('webpack');
-const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
-const { PassThrough } = require('stream');
-
+const WasmPackPlugin = require('@wasm-tool/wasm-pack-plugin');
 
 const sourceDirectory = path.resolve(__dirname, 'public');
 
 module.exports = {
-    entry: path.join(sourceDirectory, 'index.js'),
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.join(sourceDirectory, 'index.ejs')
-        }),
-        new WasmPackPlugin({
-            crateDirectory: path.resolve(__dirname, 'app')
-        })
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader',
-                ],
-            },
-            {
-                test: /\.(png|jpe?g|gif|woff2?)$/i,
-                use: [
-                    'file-loader'
-                ]
-            }
+  entry: path.join(sourceDirectory, 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(sourceDirectory, 'index.ejs'),
+    }),
+    new WasmPackPlugin({
+      crateDirectory: path.resolve(__dirname, 'app'),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
         ],
-    },
+      },
+      {
+        test: /\.(png|jpe?g|gif|woff2?)$/i,
+        use: [
+          'file-loader',
+        ],
+      },
+    ],
+  },
 };
